dff1074fbd0ed5300ac3bc3ec0d561d468a4fa97
Chokke <jd.nokk@gmail.com>
2021-02-18 18:17:28 +0100

    increible marcador

--- app/src/main/java/com/yikes/park/menus/map/MapsFragment.java
+++ app/src/main/java/com/yikes/park/menus/map/MapsFragment.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -24,6 +25,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -60,7 +62,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         // http://geojson.io/
         // https://github.com/googlemaps/android-maps-utils
 
-
+        GeoJsonLayer test;
 
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
@@ -77,43 +79,21 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        final Marker[] markerName = {null};
+        final Boolean[] firstime = {true};
         SkateParks = new ArrayList<SkatePark>();
         mMap = googleMap;
         db = FirebaseDatabase.getInstance().getReference().child("skateParks");
 
-
-        // START Get my current location
-        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
-
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 1, new LocationListener() {
-            @Override
-            public void onLocationChanged(@NonNull Location location) {
-                LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 15.3432f));
-            }
-        });
-        // END Get my current location
-
-
-
-
-        //SkatePark skatePark = new SkatePark("Favencia",41.443010789187205, 2.1714888401371972, "rampa");
-//        SkatePark skatePark2 = new SkatePark("Zona franca",41.356810315073126, 2.1411484268477743);
+//        SkatePark skatePark = new SkatePark("Favencia",41.443010789187205, 2.1714888401371972, "hotspot");
+//        SkatePark skatePark2 = new SkatePark("Zona franca",41.356810315073126, 2.1411484268477743,"marker_ramp");
 //        SkateParks.add(skatePark);
-    //    SkateParks.add(skatePark);
-
-       // db.setValue(SkateParks);
-
+//        SkateParks.add(skatePark2);
+//        db.setValue(SkateParks);
 
         db.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-
                 Log.i("logTest ", ""+dataSnapshot.getChildrenCount());
 
                 SkateParks.clear();
@@ -123,17 +103,42 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
                     SkateParks.add(skatePark);
 //                    Log.i("logTest",skatePark.getParkName());
                     LatLng park = new LatLng(skatePark.getParkLat(), skatePark.getParkLong());
-                   // LatLng park = new LatLng(41.4035806, 2.17428);
 
-                    mMap.addMarker(new MarkerOptions().position(park).title("Marker in Skate Park "+ skatePark.getParkName()).icon((BitmapDescriptorFactory.fromResource(R.drawable.marker_hotspot))));
-
+                    int markerIcon = getResources().getIdentifier(skatePark.getParkType(), "drawable", getActivity().getPackageName());
+                    mMap.addMarker(new MarkerOptions().position(park).title("Marker in Skate Park "+ skatePark.getParkName()).icon((BitmapDescriptorFactory.fromResource(markerIcon))));
 
                     //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(park, 15.3432f));
 
                 }
                 db.setValue(SkateParks);
 
+                // START Get my current location
+                locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
 
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 1, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(@NonNull Location location) {
+                        LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+                        if (firstime[0] == true){
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 15.3432f));
+                            firstime[0] = false;
+                        }
+                        if (markerName[0] !=null) {
+                            markerName[0].remove();
+                            markerName[0] =null;
+                        }
+                        if (markerName[0] ==null) {
+                            markerName[0] = mMap.addMarker(new MarkerOptions().position(myLocation).title("Your Location").icon((BitmapDescriptorFactory.fromResource(R.drawable.hotspot))));
+                        }
+
+                    }
+                });
+                // END Get my current location
             }
 
             @Override
@@ -141,18 +146,5 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
                 Log.i("logTest", "EFailed to read value.", error.toException());
             }
         });
-
-
-
-
-
-
-
-
-//        mMap = googleMap;
-//
-//        LatLng familia = new LatLng(41.4035806, 2.17428);
-//        mMap.addMarker(new MarkerOptions().position(familia).title("Marker in Sagrada Familia"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(familia));
     }
 }

9f02d48c1cde8bb2a8c88be002701b35fc6ec2fc
Don Vicente <56931522+DrGomis@users.noreply.github.com>
2021-02-21 21:53:58 +0100

    ⛑️Fixed logout. Added profile images. Cleaned the code.

--- app/src/main/java/com/yikes/park/menu/profile/ProfileFragment.java
+++ app/src/main/java/com/yikes/park/menu/profile/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.yikes.park.R;
@@ -29,17 +31,20 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        TextView txt1 = rootView.findViewById(R.id.textView4);
-
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
 
+        /* TODO: Improve everything! */
+        // Gets the avatar image from the current e-mail account
+        Glide.with(this).load(user.getPhotoUrl()).into((ImageView) rootView.findViewById(R.id.profile_image));
+
+        // Gets the id
+        TextView txt1 = rootView.findViewById(R.id.textView4);
         if (user != null) {
             txt1.setText(user.getUid());
         } else {
             txt1.setVisibility(View.GONE);
         }
-
         return rootView;
     }
 
